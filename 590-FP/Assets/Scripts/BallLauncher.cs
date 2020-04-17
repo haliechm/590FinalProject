313d732d0092bcd08aed009467a0a23cc6fd46ce
Halie Chmura <haliechm@live.unc.edu>
2020-04-17 17:26:54 -0400

    Added two rooms/steamaudio/shooting ball mechanics

---
+++ 590-FP/Assets/Scripts/BallLauncher.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallLauncher : MonoBehaviour
+{
+
+    GameObject metalBall;
+    public GameObject cam;
+   
+    void Start()
+    {
+        // go to the prefab MetalBall under Assests->Resources to change what MetalBall is/does
+        metalBall = Resources.Load("MetalBall") as GameObject;
+        
+    }
+
+    
+    void Update()
+    {
+        if(Input.GetMouseButtonDown(0)) {
+            GameObject p = Instantiate(metalBall) as GameObject;
+            p.transform.position = transform.position+cam.transform.forward*2;
+            
+          
+            Rigidbody rb = p.GetComponent<Rigidbody>();
+            rb.velocity=cam.transform.forward*15;
+            
+        }
+
+    }
+}
---
+++ 590-FP/Assets/Scripts/MovementWASD.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovementWASD : MonoBehaviour
+{
+
+      public float speed = 2f;
+      
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+
+ 
+     void Update () {
+         Vector3 pos = transform.position;
+ 
+         if (Input.GetKey ("w")) {
+             pos.z += speed * Time.deltaTime;
+         }
+         if (Input.GetKey ("s")) {
+             pos.z -= speed * Time.deltaTime;
+         }
+         if (Input.GetKey ("d")) {
+             pos.x += speed * Time.deltaTime;
+         }
+         if (Input.GetKey ("a")) {
+             pos.x -= speed * Time.deltaTime;
+         }
+             
+ 
+         transform.position = pos;
+     }
+  
+}
