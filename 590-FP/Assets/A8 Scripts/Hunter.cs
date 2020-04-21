62387da91a76e574f1dd5d1fbee3be94742860cf
Halie Chmura <haliechm@live.unc.edu>
2020-04-21 19:10:01 -0400

    Cleaned up code

--- 590-FP/Assets/A8 Scripts/Hunter.cs
+++ 590-FP/Assets/A8 Scripts/Hunter.cs
@@ -53,10 +53,7 @@ public class Hunter : MonoBehaviour
                 Debug.DrawRay(oculusCam.transform.position, oculusCam.transform.TransformDirection(Vector3.forward) * 1000, Color.white);
             }
                 
-         
-            
-            // Debug.Log("Did Hit");
-            //  Debug.Log("Object hit: " + hit.collider.gameObject);
+   
         } else {
             Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.forward) * 1000, Color.white);
        
@@ -64,8 +61,7 @@ public class Hunter : MonoBehaviour
 
         if (Input.GetKeyDown(KeyCode.Return)) {
 
-            // check to see hit object is coin object, if it is then add it to the first index of the array, etc
-            // or can just do it have an array of 20, add whatever object to the array, then when counting go through the entire array
+
             Debug.Log("_____________Key Pressed: enter");
             // Debug.Log("Object hit: " + hit.collider.gameObject);
 
@@ -73,13 +69,12 @@ public class Hunter : MonoBehaviour
             CollectibleTreasure c = hitObject.GetComponent<CollectibleTreasure>();
 
             float distanceBetweenHunterAndSphere = (transform.position - hitObject.transform.position).magnitude;
-            
-            // if(c && distanceBetweenHunterAndSphere < 1.0f) {
+  
                 if(c) {
                 numberOfObjectsCollected++;
                 Destroy(hitObject);
                 }
-            // }
+         
 
             scoreMessage.text = numberOfObjectsCollected + " ";
 
@@ -87,62 +82,9 @@ public class Hunter : MonoBehaviour
                 winMessage.text = "you win";
                 // winMessageObject.setActive();
             }
-            
 
-            // Treasure obj = (hit.collider.gameObject).GetComponent<Treasure>();
-            // Debug.Log("Object's Point Value: " + obj.pointValue);
-
-            // Debug.Log("here: " + obj.pointValue);
-
-            // score = score + obj.pointValue;
-            // // Debug.Log("score: " + score);
-            // scoreMessage.text = "Halie\r\nScore: " + score;
-
-
-            // switch(obj.pointValue) {
-            //     case 1:
-            //         // Debug.Log("coin was hit");
-            //         inventory.collectibles[0] = coin;
-            //         inventory.count[0] += 1;
-            //         break;
-            //     case 10:
-            //         // Debug.Log("Treasure Chest was hit");
-            //         inventory.collectibles[1] = treasureChest;
-            //         inventory.count[1] += 1;
-            //         break;
-            //     case 100:
-            //         // Debug.Log("Diamond was hit");
-            //         inventory.collectibles[2] = diamond;
-            //         inventory.count[2] += 1;
-            //         break;
-            //     default:
-            //         // Debug.Log("DEFAULT"); 
-            //         break;
-            // }
-
-            // if (obj.pointValue == coinPointValue) {
-            //     Debug.Log("coin");
-            // } else (obj.pointValue == treasureChestPointValue)
-            //     Debug.Log("treasure chest");
-            // } else (obj.pointValue == )
-        // updateInventory();
 
         }
-
-        // if (Input.GetKeyDown("2")) {
-        //     Debug.Log("Key Pressed: 2");
-        //     updateInventory();
-         
-        //     // inventoryMessageObject.setActive(inventoryMessageOn);
-        //     inventoryMessageOn = !inventoryMessageOn;
-        //     inventoryMessageObject.SetActive(inventoryMessageOn);
-        // }
-
-        // if(Input.GetKeyDown("3")) {
-        //     Debug.Log("Key Pressed: 3");
-        //     scoreMessageOn = !scoreMessageOn;
-        //     scoreMessageObject.SetActive(scoreMessageOn);
-        // }
 
         
 
