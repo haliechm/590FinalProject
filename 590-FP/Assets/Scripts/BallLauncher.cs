ae7d4b92f8a0d1c62473a1e5932ff470aa0817a6
Halie Chmura <haliechm@live.unc.edu>
2020-04-22 17:26:25 -0400

    Working on ball

--- 590-FP/Assets/Scripts/BallLauncher.cs
+++ 590-FP/Assets/Scripts/BallLauncher.cs
@@ -9,6 +9,10 @@ public class BallLauncher : MonoBehaviour
 // how to make ball not go past wall when directly up against wall
 // how to propagate audio off of ball?
 
+
+// add lines to show where user walked
+// just have one ball and then change position of it 
+
     GameObject metalBall;
     public GameObject cam;
    
@@ -25,7 +29,7 @@ public class BallLauncher : MonoBehaviour
         if(Input.GetMouseButtonDown(0)) {
             GameObject p = Instantiate(metalBall) as GameObject;
             // p.transform.position = transform.position+cam.transform.forward*2;
-            p.transform.position = cam.transform.position+cam.transform.forward*2;
+            p.transform.position = cam.transform.position+cam.transform.forward*1;
             
             // p.transform.position = new Vector3(cam.transform.position.x*2, cam.transform.position.y, cam.transform.position.z*2);
             Rigidbody rb = p.GetComponent<Rigidbody>();
