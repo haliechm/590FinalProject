2b7a50950cb9f1e5fcbebc9b657a27cadd17b076
Halie Chmura <haliechm@live.unc.edu>
2020-04-22 22:10:06 -0400

    Fixed audio propagation (only one ball now)

--- 590-FP/Assets/Scripts/BallLauncher.cs
+++ 590-FP/Assets/Scripts/BallLauncher.cs
@@ -13,13 +13,15 @@ public class BallLauncher : MonoBehaviour
 // add lines to show where user walked
 // just have one ball and then change position of it 
 
-    GameObject metalBall;
+    public GameObject metalBall;
     public GameObject cam;
    
     void Start()
     {
         // go to the prefab MetalBall under Assests->Resources to change what MetalBall is/does
-        metalBall = Resources.Load("MetalBall") as GameObject;
+
+        //!
+        // metalBall = Resources.Load("MetalBall") as GameObject;
         
     }
 
@@ -27,13 +29,25 @@ public class BallLauncher : MonoBehaviour
     void Update()
     {
         if(Input.GetMouseButtonDown(0)) {
-            GameObject p = Instantiate(metalBall) as GameObject;
-            // p.transform.position = transform.position+cam.transform.forward*2;
-            p.transform.position = cam.transform.position+cam.transform.forward*1;
-            
-            // p.transform.position = new Vector3(cam.transform.position.x*2, cam.transform.position.y, cam.transform.position.z*2);
-            Rigidbody rb = p.GetComponent<Rigidbody>();
+            // pause audio of BallLaunched
+            // launch ball from player position
+
+            // move metalBall position to player position
+            // launch metal ball
+
+            metalBall.transform.position = cam.transform.position;
+            metalBall.transform.position = cam.transform.position+cam.transform.forward*1;
+            Rigidbody rb = metalBall.GetComponent<Rigidbody>();
             rb.velocity=cam.transform.forward*15;
+
+            //!
+            // GameObject p = Instantiate(metalBall) as GameObject;
+        
+            // p.transform.position = cam.transform.position+cam.transform.forward*1;
+            
+           
+            // Rigidbody rb = p.GetComponent<Rigidbody>();
+            // rb.velocity=cam.transform.forward*15;
             
         }
 
