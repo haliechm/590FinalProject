89a726aa4f2a6cafa3b90b3524dbe07e564b862e
Halie Chmura <haliechm@live.unc.edu>
2020-04-22 22:12:38 -0400

    Update BallLauncher.cs

--- 590-FP/Assets/Scripts/BallLauncher.cs
+++ 590-FP/Assets/Scripts/BallLauncher.cs
@@ -6,22 +6,12 @@ public class BallLauncher : MonoBehaviour
 {
 
 
-// how to make ball not go past wall when directly up against wall
-// how to propagate audio off of ball?
-
-
-// add lines to show where user walked
-// just have one ball and then change position of it 
-
     public GameObject metalBall;
     public GameObject cam;
    
     void Start()
     {
-        // go to the prefab MetalBall under Assests->Resources to change what MetalBall is/does
-
-        //!
-        // metalBall = Resources.Load("MetalBall") as GameObject;
+       
         
     }
 
@@ -36,7 +26,6 @@ public class BallLauncher : MonoBehaviour
             // launch metal ball
 
             metalBall.transform.position = cam.transform.position;
-            metalBall.transform.position = cam.transform.position+cam.transform.forward*1;
             Rigidbody rb = metalBall.GetComponent<Rigidbody>();
             rb.velocity=cam.transform.forward*15;
 
