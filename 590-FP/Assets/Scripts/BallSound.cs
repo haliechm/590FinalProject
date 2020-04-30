3277a75060ff63a42bcf06bca74487f2745b37df
Meris Beganovic <42630924+mbeganovic3@users.noreply.github.com>
2020-04-30 16:34:27 -0400

    Fixed Tags and Audio depending on material

--- 590-FP/Assets/Scripts/BallSound.cs
+++ 590-FP/Assets/Scripts/BallSound.cs
@@ -6,7 +6,11 @@ public class BallSound : MonoBehaviour
 {
 
      public AudioClip roll;
-     public AudioClip hitWall;
+     public AudioClip WoodWall;
+     public AudioClip MetalWall;
+     public AudioClip BrickWall;
+     public AudioClip CarpetWall;
+     public AudioClip GlassWall;
      AudioSource audio;
     // Start is called before the first frame update
     void Start()
@@ -22,12 +26,29 @@ public class BallSound : MonoBehaviour
 
      void OnCollisionEnter(Collision other){
 
-         float impactVelocity = other.relativeVelocity.magnitude;
+        float impactVelocity = other.relativeVelocity.magnitude;
 
-        if(other.gameObject.tag == "InvisibleWall"){
-          audio.clip = hitWall;
+        // Wall Hit Sound
+        if(other.gameObject.tag == "WoodWall"){
+          audio.clip = WoodWall;
+          audio.Play(0);
+        }else if(other.gameObject.tag == "MetalWall"){
+          audio.clip = MetalWall;
+          audio.Play(0);
+        }else if(other.gameObject.tag == "BrickWall"){
+          audio.clip = BrickWall;
+          audio.Play(0);
+        }else if(other.gameObject.tag == "CarpetWall"){
+          audio.clip = CarpetWall;
+          audio.Play(0);
+        }else if(other.gameObject.tag == "GlassWall"){
+          audio.clip = GlassWall;
           audio.Play(0);
         }
+
+
+
+        // Rolling Sound
         if(other.gameObject.tag == "HardwoodFloor"){
             // linearly interpolates 
           audio.volume = Mathf.Lerp(0.1f, 1.0f, impactVelocity/(1.0f-0.1f));
